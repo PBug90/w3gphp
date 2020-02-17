eec8bb56a6ec75dbc6978baf4f79dcbd612f5246
Patrick Schäfer <padjon@users.noreply.github.com>
2020-02-17 18:07:01 +0100

    fix of long name truncates in reforged

--- parser/w3g-julas.php
+++ parser/w3g-julas.php
@@ -288,6 +288,14 @@ class replay
                 $temp = unpack('cname_length', $this->data);
                 $this->data = substr($this->data, 1);
                 
+              	$playerName = substr($this->data, 0, $temp["name_length"]);
+                foreach($this->players as $key=>$player) {
+                    if(substr($playerName, 0, strlen($player['name'])) == $player['name']) {
+                        $this->players[$key]['name'] = $playerName;
+                        break;
+                    }
+                }
+              
                 $this->data = substr($this->data, $temp["name_length"]+1);
                 
                 $temp = unpack('cclan_length', $this->data);
